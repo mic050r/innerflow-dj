72b948bedfd231ea37312f829d31eae1538929bf
mic050r <s2216@e-mirim.hs.kr>
2024-05-26 02:07:42 +0900

    💄 [style] (#3) 로그인 화면 구현

--- InnerFlow/settings.py
+++ InnerFlow/settings.py
@@ -42,8 +42,8 @@ ROOT_URLCONF = 'InnerFlow.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+         'DIRS': [os.path.join(BASE_DIR, 'InnerFlow', 'templates')],
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -108,7 +108,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'InnerFlow', 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
