204c46d4095b4eb3b15d92d285a4487d614faa68
mic050r <s2216@e-mirim.hs.kr>
2024-06-29 02:38:23 +0900

    ✨ [Feat] (#5) 홈에 나의 업적, 목표 진행상황 추가

--- InnerFlow/views.py
+++ InnerFlow/views.py
@@ -29,11 +29,42 @@ load_dotenv()
 def index(request):
     return render(request, 'index.html')
 
+
 def home(request):
+    kakao_id = request.session.get('kakao_id')
+    kakao_nickname = request.session.get('kakao_nickname')
+
     with open('./InnerFlow/static/data/info.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
     print(data)
-    return render(request, 'home.html', {'data': json.dumps(data)})
+
+    # 업적 데이터 가져오기
+    achievements = Achievement.objects.filter(user__kakao_id=kakao_id)
+    keywords = []
+    for achievement in achievements:
+        keywords.extend(achievement.keyword.split('/'))
+
+    # 목표 데이터 가져오기 (최신순으로 3개)
+    goals = Goal.objects.filter(user__kakao_id=kakao_id).order_by('-created_at')[:3]
+    goal_stats = []
+    for goal in goals:
+        todos = Todo.objects.filter(goal=goal)
+        total_todos = todos.count()
+        completed_todos = todos.filter(checked=True).count()
+        completion_rate = (completed_todos / total_todos) * 100 if total_todos > 0 else 0
+        goal_stats.append({
+            'goal': goal,
+            'total_todos': total_todos,
+            'completed_todos': completed_todos,
+            'completion_rate': completion_rate,
+        })
+
+    return render(request, 'home.html', {
+        'data': json.dumps(data),  # 기존의 data 추가
+        'keywords': keywords,
+        'goal_stats': goal_stats,
+        'nickname': kakao_nickname
+    })
 
 
 # 카카오 로그인 페이지로 리다이렉트하는 함수
@@ -103,6 +134,7 @@ def kakao_callback(request):
         request.session['access_token'] = str(access)
         request.session['refresh_token'] = str(refresh)
         request.session['kakao_id'] = str(kakao_id)
+        request.session['kakao_nickname']  = str(kakao_nickname)
         print(request.session.get('kakao_id'))
 
         return redirect('/home/')  # 로그인 성공 후 /home으로 리다이렉트
