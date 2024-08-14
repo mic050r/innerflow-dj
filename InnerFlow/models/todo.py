0fbd98c1bb51ef1a18e0dc164a1529abf270fa44
mic050r <s2216@e-mirim.hs.kr>
2024-08-14 14:46:51 +0900

    💡 [Comment] User model 주석 추가

--- InnerFlow/models/todo.py
+++ InnerFlow/models/todo.py
@@ -1,6 +1,7 @@
 from django.db import models
 from .goal import Goal
 
+
 class Todo(models.Model):
     todo_id = models.AutoField(primary_key=True)
     goal = models.ForeignKey(Goal, on_delete=models.CASCADE)
--- InnerFlow/models/user.py
+++ InnerFlow/models/user.py
@@ -1,30 +1,33 @@
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, Group, Permission
 from django.db import models
 
-
+# UserManager : 사용자 생성 로직을 정의하는 커스텀 매니저 클래스
 class UserManager(BaseUserManager):
+    # 일반 사용자 생성을 위한 메소드
     def create_user(self, kakao_id, password=None, **extra_fields):
         if not kakao_id:
-            raise ValueError('The Kakao ID must be set')
-        user = self.model(kakao_id=kakao_id, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
+            raise ValueError('The Kakao ID must be set') # 카카오 ID x -> 예외 발생
+        user = self.model(kakao_id=kakao_id, **extra_fields) # user 모델 인스턴스 생성
+        user.set_password(password) # 비밀번호 해싱하여 설정
+        user.save(using=self._db) # db에 저장
         return user
 
+    # 슈퍼유저(관리자) 생성을 위한 메서드
     def create_superuser(self, kakao_id, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True) # 권리자 권한
+        extra_fields.setdefault('is_superuser', True) # 슈퍼유저 권한 설정
 
         return self.create_user(kakao_id, password, **extra_fields)
 
-
+# User 모델
 class User(AbstractBaseUser, PermissionsMixin):
-    kakao_id = models.CharField(max_length=255, unique=True)
-    profile = models.CharField(max_length=500, blank=True)
-    nickname = models.CharField(max_length=50, blank=True)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
+    kakao_id = models.CharField(max_length=255, unique=True) # 사용자 고유 kakao ID
+    profile = models.CharField(max_length=500, blank=True) # 사용자 프로필 정보
+    nickname = models.CharField(max_length=50, blank=True) # 사용자 닉네임
+    is_active = models.BooleanField(default=True) # 계정 활성 상태
+    is_staff = models.BooleanField(default=False) # 관리자 권한 여부
 
+    # Django의 기본 그룹 기능을 사용하기 위한 필드
     groups = models.ManyToManyField(
         Group,
         related_name='customuser_groups',
@@ -40,10 +43,11 @@ class User(AbstractBaseUser, PermissionsMixin):
         verbose_name='user permissions',
     )
 
+    # UserMangage를 기본 매니저로 사용
     objects = UserManager()
 
-    USERNAME_FIELD = 'kakao_id'
-    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'kakao_id' # 로그인 시 사용될 필드 지정
+    REQUIRED_FIELDS = [] # 슈퍼 유저 생성시 추가로 필요한 필드 목록
 
     def __str__(self):
-        return str(self.kakao_id)
+        return str(self.kakao_id) # User 객체를 문자열로 변환할 때 kakao id를 반환
